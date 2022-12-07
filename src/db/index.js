3ee3438960c972148bfa7bd9abdd8ad87adfa2d6
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-07 12:14:42 -0800

    Testing

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
 const pg = require("pg");
-
+console.log("DB URL:", process.env.DATABASE_URL)
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
